44ea8367f9c47885a3a53a17cf678a6fefd88775
John Belmonte <john.belmonte@groove-x.com>
2020-09-17 22:45:56 +0900

    fix premature exit of AsyncDictionary test

--- tests/test_async_dictionary.py
+++ tests/test_async_dictionary.py
@@ -1,24 +1,8 @@
 import trio
-from trio_util import AsyncDictionary
+from trio_util import AsyncDictionary, wait_all
 
-async def test_async_dictionary(nursery, autojump_clock):
-    async def reader(d):
-        assert await d.get_wait('bar') == 99
-        assert 'bar' in d
-        assert await d.pop_wait('baz') == 'done'
-        assert 'baz' not in d
-        with trio.move_on_after(.1):
-            await d.get_wait('baz')
-            assert False
 
-    async def writer(d):
-        await trio.sleep(.1)
-        assert d.is_waiting('bar')
-        d['bar'] = 99
-        await trio.sleep(.1)
-        assert d.is_waiting('baz')
-        d['baz'] = 'done'
-
+async def test_async_dictionary(autojump_clock):
     d = AsyncDictionary(foo=10)
     assert len(d) == 1
     assert 'foo' in d
@@ -27,5 +11,22 @@ async def test_async_dictionary(nursery, autojump_clock):
     assert not d.is_waiting('foo')
     del d['foo']
     assert not d
-    nursery.start_soon(reader, d)
-    nursery.start_soon(writer, d)
+
+    async def reader():
+        assert await d.get_wait('bar') == 99
+        assert 'bar' in d
+        assert await d.pop_wait('baz') == 'done'
+        assert 'baz' not in d
+        with trio.move_on_after(.1):
+            await d.get_wait('baz')
+            assert False
+
+    async def writer():
+        await trio.sleep(.1)
+        assert d.is_waiting('bar')
+        d['bar'] = 99
+        await trio.sleep(.1)
+        assert d.is_waiting('baz')
+        d['baz'] = 'done'
+
+    await wait_all(reader, writer)

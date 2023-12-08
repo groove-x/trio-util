684e15ae4380934942551edc09b44de5664255e4
Spencer Brown <spencerb21@live.com>
2023-12-08 17:04:19 +1000

    Add types to _iterators

--- src/trio_util/_iterators.py
+++ src/trio_util/_iterators.py
@@ -1,9 +1,12 @@
-from collections.abc import AsyncIterator
+from typing import AsyncIterator, Generic, TypeVar
 
 import trio
 
 
-class iter_move_on_after(AsyncIterator):
+T = TypeVar("T")
+
+
+class iter_move_on_after(AsyncIterator[T], Generic[T]):
     """async iterator adapter that stops if an iteration exceeds timeout
 
     The timeout is a duration in seconds.
@@ -16,18 +19,18 @@ class iter_move_on_after(AsyncIterator):
 
     __slots__ = ['_ait', '_timeout']
 
-    def __init__(self, timeout, ait):
+    def __init__(self, timeout: float, ait: AsyncIterator[T]) -> None:
         self._ait = ait
         self._timeout = timeout
 
-    async def __anext__(self):
+    async def __anext__(self) -> T:
         with trio.move_on_after(self._timeout):
             x = await self._ait.__anext__()
             return x
         raise StopAsyncIteration
 
 
-class iter_fail_after(AsyncIterator):
+class iter_fail_after(AsyncIterator[T], Generic[T]):
     """async iterator adapter that raises trio.TooSlowError if an iteration exceeds timeout
 
     The timeout is a duration in seconds.
@@ -40,10 +43,10 @@ class iter_fail_after(AsyncIterator):
 
     __slots__ = ['_ait', '_timeout']
 
-    def __init__(self, timeout, ait):
+    def __init__(self, timeout: float, ait: AsyncIterator[T]) -> None:
         self._ait = ait
         self._timeout = timeout
 
-    async def __anext__(self):
+    async def __anext__(self) -> T:
         with trio.fail_after(self._timeout):
             return await self._ait.__anext__()

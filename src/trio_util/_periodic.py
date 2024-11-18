8931eb334a7ac77ae5570e78b30ff4a5a2c2ebd5
John Belmonte <john.belmonte@groove-x.com>
2024-11-17 17:57:25 -0800

    fix tests/lint/type_check for recent Trio versions (#28)
    Runtime isn't changed. Just exclude exception utilities from the static
    build checks. (The exception utilities have a runtime dependency on the
    defunct trio.MultiError, but if you don't use them, it's fine.)
    
    also:
      * upgrade test dependencies
      * add typing to periodic()

--- src/trio_util/_periodic.py
+++ src/trio_util/_periodic.py
@@ -1,6 +1,9 @@
+from typing import AsyncGenerator, Tuple, Optional
+
 import trio
 
-async def periodic(period):
+
+async def periodic(period) -> AsyncGenerator[Tuple[float, Optional[float]], None]:
     """Yield `(elapsed_time, delta_time)` with an interval of `period` seconds.
 
     For example, to loop indefinitely with a period of 1 second, accounting
@@ -14,8 +17,10 @@ async def periodic(period):
     On the first iteration, `delta_time` will be `None`.
     """
     t0 = trio.current_time()
-    t_last, t_start = None, t0
+    t_start = t0
+    delta_time: Optional[float] = None
     while True:
-        yield t_start - t0, t_start - t_last if t_last is not None else None
+        yield t_start - t0, delta_time
         await trio.sleep_until(t_start + period)
         t_last, t_start = t_start, trio.current_time()
+        delta_time = t_start - t_last
